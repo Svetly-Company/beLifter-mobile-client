2187f20a6927750add7108f3e3a153017b7bba73
JulipoDom <jdnn2006@gmail.com>
2024-05-28 10:11:25 -0300

    feat: Treino

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import './src/styles/global.css'
+import { Home } from './src/pages/Home';
+import { Treino } from './src/pages/Treino';
+
+import { 
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium
+ } from '@expo-google-fonts/montserrat';
+
+ import {
+  IBMPlexSans_100Thin,
+  IBMPlexSans_100Thin_Italic,
+  IBMPlexSans_200ExtraLight,
+  IBMPlexSans_200ExtraLight_Italic,
+  IBMPlexSans_300Light,
+  IBMPlexSans_300Light_Italic,
+  IBMPlexSans_400Regular,
+  IBMPlexSans_400Regular_Italic,
+  IBMPlexSans_500Medium,
+  IBMPlexSans_500Medium_Italic,
+  IBMPlexSans_600SemiBold,
+  IBMPlexSans_600SemiBold_Italic,
+  IBMPlexSans_700Bold,
+  IBMPlexSans_700Bold_Italic,
+} from '@expo-google-fonts/ibm-plex-sans';
+
+export default function App() {
+  const [fontsLoaded] = useFonts(
+    {
+      Montserrat_400Regular,
+      Montserrat_500Medium,
+      IBMPlexSans_400Regular,
+      IBMPlexSans_500Medium
+    }
+  )
+  return (
+    <>
+    <StatusBar backgroundColor={'#111112'} style="light" translucent/>
+    {
+      fontsLoaded? <Treino/> : ''
+    }
+    
+    </>
+  );
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+import { green } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+
+export const colors = {
+  gray: {
+    800: "#111112",
+    950: "#0C0C0C",
+    1000: "#3E3E3E"
+  },
+  neutral: {
+    900: "#151415"
+  },
+  green: {
+    800: "#00592E"
+  }
+}
