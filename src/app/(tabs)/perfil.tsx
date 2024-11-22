cc9ae6e1441b1275d01cafb23e98a848912dc49e
Riquezlvs <riquezlvs@gmail.com>
2024-11-22 20:15:17 -0300

    Mudanças no PerfildoCliente

--- src/app/(tabs)/perfil.tsx
+++ src/app/(tabs)/perfil.tsx
@@ -1,307 +1,179 @@
-import { ImageBackground, Text, TouchableOpacity, View, Image } from "react-native";
-import { ScrollView } from "react-native-virtualized-view";
-import { SafeAreaView } from "react-native-safe-area-context";
-import HeaderHome from "../../components/HeaderHome";
-import { BoxModel } from "../../components/BoxModel";
-import { Link, router, useNavigation } from 'expo-router';
-import { getUserData } from "../../storage/userData/getUserData";
-import { useEffect, useState } from "react";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Image, ScrollView, FlatList, ImageSourcePropType } from "react-native";
 import { CaretLeft, UploadSimple, DotsThree, CaretDown, Fire, Footprints, Gauge, CalendarBlank, TrendUp, Barbell, PersonSimple } from "phosphor-react-native";
-import { userStorage } from "../../storage/zustand/store";
-import { Post } from "../../components/Post";
-import { FlatList } from "react-native-gesture-handler";
-import { ImageSourcePropType } from "react-native";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { userStorage } from "../../storage/zustand/store";
+import { Post } from "../../components/Post";
 
+interface PostObject { 
+  id: number; 
+  content: string; 
+  mediaUrl: ImageSourcePropType 
+}
+
+interface StatisticType {
+  label: string;
+  value: string;
+  Icon: React.ReactNode;
+}
+
+interface PanelItemType {
+  label: string;
+  Icon: React.ReactNode;
+}
+
+const statisticsData: StatisticType[] = [
+  { label: "Distância", value: "20km", Icon: <Gauge size={32} color="#00BF63" /> },
+  { label: "Passos", value: "20km", Icon: <Footprints size={32} color="#00BF63" /> },
+  { label: "Kcal", value: "20km", Icon: <Fire size={32} color="#00BF63" /> }
+];
+
+const panelItemsData: PanelItemType[] = [
+  { label: "Estatísticas", Icon: <TrendUp size={20} color="#00BF63" /> },
+  { label: "Exercícios", Icon: <Barbell size={20} color="#00BF63" /> },
+  { label: "Medições", Icon: <PersonSimple size={20} color="#00BF63" /> },
+  { label: "Calendários", Icon: <CalendarBlank size={20} color="#00BF63" /> }
+];
 
 export default function Profile() {
-
-
-  interface PostObject { id: number, content: string, mediaUrl: ImageSourcePropType }
-
-  const user = userStorage((state) => state.user)
-
-  const [posts, setPosts] = useState<PostObject[]>([])
+  const user = userStorage((state) => state.user);
+  const [posts, setPosts] = useState<PostObject[]>([]);
 
   const { data: userPost, refetch } = useQuery('posts/me', async () => {
-    return await axios.get('https://belifter-server.onrender.com/profile/me',
-      {
-        headers: {
-          'Authorization': `Bearer ${user.token}`
-        }
-      })
-  })
-
-  // const arr = [{
-  //   id: 1,
-  //   content: 'why u so obssed',
-  //   mediaUrl: posts[0].mediaUrl
-  // }]
-
+    const response = await axios.get('https://belifter-server.onrender.com/profile/me', {
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    });
+    return response.data.posts;
+  });
 
   useEffect(() => {
-    if (userPost && userPost.data) {
-      setPosts(userPost.data.posts)
+    if (userPost) {
+      setPosts(userPost);
     }
+  }, [userPost]);
 
-  })
+  const renderStatistic = (stat: StatisticType) => (
+    <View className="w-2/6 items-center py-4">
+      {stat.Icon}
+      <Text className="text-white font-ibmRegular">{stat.label}</Text>
+      <Text className="text-white font-ibmRegular">{stat.value}</Text>
+    </View>
+  );
 
-  console.log(user.token)
+  const renderPanelItem = (item: PanelItemType) => (
+    <View className="bg-neutral-900 w-5/12 h-14 gap-2 flex-row items-center justify-center rounded-2xl">
+      {item.Icon}
+      <Text className="font-ibmRegular text-base text-white py-4">{item.label}</Text>
+    </View>
+  );
 
   return (
-      <View className="flex-1 bg-black">
+    <View className="flex-1 bg-black">
+      <View className="h-96 rounded-b-3xl flex flex-col bg-neutral-900">
+        <View className="h-12"></View>
 
-        <View className="h-96 rounded-b-3xl flex flex-col bg-neutral-900">
+        {/* Header with centered text and icons */}
+        <View className="flex h-min flex-row mt-3 items-center justify-between px-4">
+          <TouchableOpacity onPress={() => console.log("Back pressed")}>
+            <CaretLeft size={32} color="#00BF63" />
+          </TouchableOpacity>
 
-          <View className="h-12"></View>
-
-          <View className=" flex h-min flex-row mt-3 items-center">
-
-            <View className="h-2/4 mb-2 w-4/12">
-              <CaretLeft size={32} color="#00BF63" />
-            </View>
-
-            <View className="flex flex-row gap-8">
-              <View className="w-3/6">
-                <Text className="font-ibmMedium text-xl mt-2 text-white capitalize">{user.name}</Text>
-              </View>
-
-              <View className="w-2/6 flex-row gap-3 mt-2">
-                <UploadSimple size={28} color="#00BF63" />
-                <DotsThree size={28} color="#00BF63" />
-              </View>
-            </View>
-
-
-
-          </View>
-
-          <View className="flex mt-3 h-4/6 gap-3">
-
-            <View className="flex flex-row">
-              <View className=" items-center w-2/6 ">
-                <Image source={require('../../assets/moca.jpg')} className="w-24 h-24 rounded-full mr-5" />
-              </View>
-
-              <View className="flex flex-row gap-5 py-4">
-
-                <View className="flex gap-2">
-
-                  <View>
-                    <Text className="font-ibmMedium text-white font-extralight">Treinamentos</Text>
-                  </View>
-
-                  <View className="items-center">
-                    <Text className="font-ibmMedium text-white">130</Text>
-                  </View>
-
-                </View>
-
-
-                <View className="flex gap-2">
-
-                  <View>
-                    <Text className="font-ibmMedium text-white font-extralight">Seguidores</Text>
-                  </View>
-
-                  <View className="items-center">
-                    <Text className="font-ibmMedium text-white">2K</Text>
-                  </View>
-
-                </View>
-
-                <View className="flex gap-2">
-                  <View>
-                    <Text className="font-ibmMedium text-white font-extralight">Seguindo</Text>
-                  </View>
-
-                  <View className="items-center">
-                    <Text className="font-ibmMedium text-white">200</Text>
-                  </View>
-                </View>
-
-              </View>
-
-            </View>
-
-            <View className="flex flex-1 gap-1">
-
-              <View className="ml-8 flex gap-2">
-
-                <View>
-                  <Text className="font-ibmMedium text-lg text-white">{user.name}</Text>
-                </View>
-
-                <View>
-                  <Text className="font-ibmRegular text-lg text-white">Em briga de saci, todo chute é voadora!  </Text>
-                </View>
-
-
-              </View>
-
-              <View className="items-center flex w-full">
-
-                <TouchableOpacity style={{ backgroundColor: "#00BF63", height: 46, width: 360, alignItems: "center", justifyContent: "center", marginTop: 28, borderRadius: 40, }}>
-                  <Text className="text-white">Editar perfil</Text>
-                </TouchableOpacity>
-
-              </View>
-
-
+          <View className="flex flex-row gap-8 items-center">
+            <Text className="font-ibmMedium text-xl text-white capitalize">{user.name}</Text>
+            <View className="flex-row gap-3">
+              <UploadSimple size={28} color="#00BF63" />
+              <DotsThree size={28} color="#00BF63" />
             </View>
           </View>
-
         </View>
 
-        <ScrollView className=" bg-black flex-1 ">
-        <View className="flex flex-row h-1/12 ">
-
-          <View className="w-2/6 items-center py-4">
-            <Text className="text-white text-xl font-ibmMedium">Estatísticas</Text>
+        {/* Profile Details Section */}
+        <View className="flex mt-3 gap-3 mx-8">
+        <View className="flex w-full flex-row justify-between items-center">
+          <Image source={require('../../assets/moca.jpg')} className="w-24 h-24 rounded-full mr-5" />
+          <View className="flex flex-row gap-5 py-4">
+            <View className="flex gap-2 items-center">
+              <Text className="font-ibmMedium text-white font-extralight">Treinamentos</Text>
+              <Text className="font-ibmMedium text-white">130</Text>
+            </View>
+            <View className="flex gap-2 items-center">
+              <Text className="font-ibmMedium text-white font-extralight">Seguidores</Text>
+              <Text className="font-ibmMedium text-white">2K</Text>
+            </View>
+            <View className="flex gap-2 items-center">
+              <Text className="font-ibmMedium text-white font-extralight">Seguindo</Text>
+              <Text className="font-ibmMedium text-white">200</Text>
+            </View>
           </View>
-
-          <View className="w-5/12">
-
-          </View>
-
-          <View className=" flex flex-row py-4 flex-1 gap-3">
-            <Text className="text-white text-xl font-ibmMedium">Mês</Text>
-            <CaretDown size={32} color="#00BF63" />
-          </View>
-
         </View>
 
-        <View>
-
-          <View className="items-center h-28">
-
-            <View className="bg-neutral-900 flex flex-1 flex-row rounded-2xl w-5/6">
-
-              <View className="w-2/6 items-center flex py-4">
-
-                <View>
-                  <Gauge size={32} color="#00BF63" />
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">Distância</Text>
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">20km</Text>
-                </View>
-
-              </View>
-
-              <View className="w-2/6 items-center flex py-4">
-
-                <View>
-                  <Footprints size={32} color="#00BF63" />
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">Passos</Text>
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">20km</Text>
-                </View>
-
-              </View>
-
-              <View className="w-2/6 items-center flex py-4">
-                <View>
-                  <Fire size={32} color="#00BF63" />
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">Kcal</Text>
-                </View>
-
-                <View>
-                  <Text className="text-white font-ibmRegular">20km</Text>
-                </View>
-              </View>
-
-
-            </View>
-
-          </View>
-
-          <View className="gap-4">
-
-            <View className="ml-10 mt-3">
-
-              <Text className="text-white text-xl font-ibmMedium">Painel</Text>
-
-            </View>
-
-            <View className="flex gap-5 items-center">
-
-              <View className="flex flex-row gap-5">
-
-                <View className="bg-neutral-900 w-5/12 h-14 gap-2 flex-row items-center justify-center rounded-2xl ">
-
-                  <TrendUp size={20} color="#00BF63" />
-
-                  <Text className="font-ibmRegular text-base text-white py-4">Estatísticas</Text>
-
-                </View>
-
-                <View className="bg-neutral-900 w-5/12 h-14 items-center gap-2 justify-center flex-row rounded-2xl ">
-
-                  <Barbell size={20} color="#00BF63" />
-
-                  <Text className="font-ibmRegular text-base text-white py-4">Exercícios</Text>
-
-                </View>
-
-              </View>
-
-              <View className="flex flex-row gap-5">
-
-                <View className="bg-neutral-900 w-5/12 h-14 items-center flex-row gap-2 justify-center  rounded-2xl ">
-
-                  <PersonSimple size={20} color="#00BF63" />
-
-                  <Text className="font-ibmRegular text-base text-white py-4">Medições</Text>
-
-                </View>
-
-                <View className="bg-neutral-900 w-5/12 h-14 flex-row items-center gap-2 justify-center  rounded-2xl ">
-
-                  <CalendarBlank size={20} color="#00BF63" />
-
-                  <Text className="font-ibmRegular text-base text-white py-4">Calendários</Text>
-
-                </View>
-
-              </View>
-
-            </View>
-
-          </View>
-
-
-
+        {/* User's Name and Bio */}
+        <View className="ml-0 flex items-start gap-2">
+          <Text className="font-ibmMedium text-lg text-white">{user.name}</Text>
+          <Text className="font-ibmRegular text-lg text-white">Em briga de saci, todo chute é voadora! 🤞✌💋🌹</Text>
         </View>
 
-        <FlatList
+        {/* Edit Profile Button */}
+        <View className="flex items-center mt-5">
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#00BF63",
+              height: 46,
+              width: 380,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 40
+            }}
+          >
+            <Text className="text-white">Editar perfil</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      </View>
+
+      <ScrollView className="bg-black flex-1">
+      <View className="flex flex-row justify-between mx-8">
+        <Text className="text-white text-xl font-ibmMedium py-4">Estatísticas</Text>
+        <View className="flex flex-row py-4 gap-3 items-center">
+          <Text className="text-white text-xl font-ibmMedium">Mês</Text>
+          <CaretDown size={32} color="#00BF63" />
+        </View>
+      </View>
+
+        <View className="items-center h-28">
+          <View className="bg-neutral-900 flex flex-1 flex-row rounded-2xl w-5/6">
+            {statisticsData.map(renderStatistic)}
+          </View>
+        </View>
+
+        <View className="gap-4">
+          <Text className="ml-10 mt-3 text-white text-xl font-ibmMedium">Painel</Text>
+          <View className="flex gap-5 items-center">
+            <View className="flex flex-row gap-5">
+              {panelItemsData.slice(0, 2).map(renderPanelItem)}
+            </View>
+            <View className="flex flex-row gap-5">
+              {panelItemsData.slice(2).map(renderPanelItem)}
+            </View>
+          </View>
+        </View>
+
+        <View className="gap-4">
+          <Text className="ml-10 mt-3 text-white text-xl font-ibmMedium">Postagens</Text>
+          <FlatList
           data={posts}
           renderItem={({ item }) => <Post image={item.mediaUrl} content={item.content} id={item.id} author={user.name} refetch={refetch} />}
           keyExtractor={item => item.id.toString()}
           style={{ paddingBottom: 100 }}
-        />
+          />
+        </View>
 
-        </ScrollView>
-
-      </View>
-
-
-
-
-
-  )
+        
+      </ScrollView>
+    </View>
+  );
 }
